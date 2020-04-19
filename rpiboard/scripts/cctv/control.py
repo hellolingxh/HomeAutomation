1fac38bb573497fabf1326a5a9998ca8f1a96c06
Xiaohui <hellolingxh@gmail.com>
2020-04-19 18:02:03 +0100

    ixed the bug when rutn the camera, the door camera and cctv camera always mixed, so add the -d argument to specific the /dev/video1 to USB camera, the /dev/video0 to raspcam

--- rpiboard/scripts/cctv/control.py
+++ rpiboard/scripts/cctv/control.py
@@ -3,7 +3,7 @@
 import os
 
 def cctvOn():
-    os.system("nohup mjpg_streamer -i 'input_uvc.so -r 640x480 -f 20' -o 'output_http.so -p 8080 -w /tmp/www' &")
+    os.system("nohup mjpg_streamer -i 'input_uvc.so -d /dev/video1 -r 640x480 -f 20' -o 'output_http.so -p 8080 -w /tmp/www' &")
 
 def cctvOff():
     os.system('pkill mjpg_streamer')
--- rpiboard/scripts/door/camera/control.py
+++ rpiboard/scripts/door/camera/control.py
@@ -2,7 +2,7 @@
 import os
 
 def cameraOn():
-    os.system("nohup mjpg_streamer -i 'input_raspicam.so' -o 'output_http.so -p 8088 -w /tmp/www' &")
+    os.system("nohup mjpg_streamer -i 'input_raspicam.so -d /dev/video0' -o 'output_http.so -p 8088 -w /tmp/www' &")
 
 def cameraOff():
     os.system('pkill mjpg_streamer')
